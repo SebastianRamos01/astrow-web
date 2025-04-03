bbb8a646477156c8fcefbeb19f650f5a534a2087
SebastianRamos01 <sebasr.sr@hotmail.com>
2025-04-02 23:21:22 -0300

    first commit

---
+++ src/data/productsMen.ts
@@ -0,0 +1,45 @@
+export const ProductsMen = [
+    {
+        id: 1,
+        name: 'Rebel Denim Jacket',
+        price: '$ 85.00',
+        img: 'Pexels Photo by Raymond Petrik.png',
+        featured: false,
+    },
+    {
+        id: 2,
+        name: 'Core Street Pullover',
+        price: '$ 52.00',
+        img: 'Pexels Photo by Clem Onojeghuo.png',
+        featured: false,
+    },
+    {
+        id: 3,
+        name: 'Chettah Fisher Cap autumn 2025',
+        price: '$ 120.00',
+        img: 'Pexels Photo by Reza Kargar.png',
+        featured: true,
+    },
+    {
+        id: 4,
+        name: 'Classic Edge Shirt',
+        price: '$ 42.00',
+        img: 'Pexels Photo by Vurzie  Kim.png',
+        featured: false,
+    },
+    {
+        id: 5,
+        name: 'Leather Hat autumn 2025',
+        price: '$ 25.00',
+        img: 'Pexels Photo by Luca Nardone.png',
+        featured: true,
+    },
+    {
+        id: 6,
+        name: 'Vintage Varsity',
+        price: '$ 95.00',
+        img: 'Pexels Photo by Chichebe Chidiebere.png',
+        featured: false,
+    },
+
+]
---
+++ src/data/productsWomen.ts
@@ -0,0 +1,44 @@
+export const ProductsWomen = [
+    {
+        id: 1,
+        name: 'Cozy Luxe Cardigan',
+        price: '$ 60.00',
+        img: 'Pexels Photo by cottonbro studio.png',
+        featured: false,
+    },
+    {
+        id: 2,
+        name: 'Long Skirt atumn 2025',
+        price: '$ 240.00',
+        img: 'Pexels Photo by Elias de Carvalho.png',
+        featured: true,
+    },
+    {
+        id: 3,
+        name: 'Retro Denim Crop',
+        price: '$ 75.00',
+        img: 'Pexels Photo by Ivan Oboleninov.png',
+        featured: false,
+    },
+    {
+        id: 4,
+        name: 'Silk Essence Wrap',
+        price: '$ 45.00',
+        img: 'Pexels Photo by Jessica Nunes.png',
+        featured: false,
+    },
+    {
+        id: 5,
+        name: 'Timeless Muse Dress',
+        price: '$ 85.00',
+        img: 'Pexels Photo by Konstantin Mishchenko.png',
+        featured: false,
+    },
+    {
+        id: 6,
+        name: 'Pink Wide Glasses atumn 2025',
+        price: '$ 45.00',
+        img: 'Pexels Photo by Ron Lach.png',
+        featured: true,
+    },
+]
